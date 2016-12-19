0d33b8f1cbfc0339ef4898fc4bb68ec60fa7c534
Christopher Vundi <christophervundi@gmail.com>
2016-12-19 16:00:58 +0300

    add spec for deleting projects

---
+++ spec/features/delete_project_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Users can delete projects" do
+  scenario "Successfully" do
+    project = FactoryGirl.create(:project, name: "Sublime Text")
+    visit project_url(project)
+    click_link "Delete Project"
+    expect(page).to have_content "Project was deleted"
+    expect(page.current_url).to eq projects_url
+    expect(page).to have_no_content "Sublime Text"
+  end
+end
