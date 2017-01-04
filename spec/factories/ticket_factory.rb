186cc7ce72e04b61c4780dc26a3f15a303a30eb2
Christopher Vundi <christophervundi@gmail.com>
2017-01-04 12:54:02 +0300

    Add spec for viewing tickets

---
+++ spec/factories/ticket_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :ticket do
+    name "Example ticket"
+    description "An example ticket. Nothing more. I wish there was more"
+  end
+
+end
---
+++ spec/features/view_tickets_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Users can view tickets" do
+  before do
+    sublime = FactoryGirl.create(:project, name: "sublime")
+    FactoryGirl.create(:ticket, project: sublime, name: "first ticket", description: "This is the description for the first ticket")
+    ie = FactoryGirl.create(:project, name: "ie")
+    FactoryGirl.create(:ticket, project: ie, name: "ie ticket", description: "This is the description for the ie ticket")
+
+    visit "/"
+  end
+
+  scenario "For a given project" do
+    click_link "sublime"
+    expect(page).to have_content "first ticket"
+    expect(page).to_not have_content "ie ticket"
+
+    click_link "first ticket"
+
+    within("#ticket h2") do
+      expect(page).to have_content "first ticket"
+    end
+
+    expect(page).to have_content "This is the description for the first ticket"
+
+  end
+
+end
