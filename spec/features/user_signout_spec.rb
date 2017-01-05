02476be1d7ced3b572a3f26eb41f92e5ec17e37d
Christopher Vundi <christophervundi@gmail.com>
2017-01-05 11:42:19 +0300

    Add devise gor authentication

---
+++ spec/features/user_signout_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "Logged in users can sign out" do
+
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    login_as(user)
+  end
+
+  scenario "successfully" do
+    visit "/"
+    click_link "Sign Out"
+    expect(page).to have_content "Signed out successfully"
+  end
+end
