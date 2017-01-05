e691cc6b85abf114b6a9758fd0eaa671ec9c7bae
Christopher Vundi <christophervundi@gmail.com>
2017-01-05 14:04:33 +0300

    Add amin column to users

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password "password"
+
+    trait :admin do
+      admin true
+    end
   end
 
 end
