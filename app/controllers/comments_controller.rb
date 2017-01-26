0f78796c17dd12c90326b200b0f7b109f5b0bffe
Christopher Vundi <chris.vundi@fire-engine-red.com>
2017-01-26 17:34:21 +0300

    Prevent unauthorised users from changing states

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
   before_action :find_ticket
+
   def create
-    @comment = @ticket.comments.build(comment_params)
+    whitelisted_params = comment_params
+
+    unless policy(@ticket).change_state?
+      whitelisted_params.delete(:state_id)
+    end
+
+    @comment = @ticket.comments.build(whitelisted_params)
     @comment.user = current_user
     authorize @comment, :create?
 
